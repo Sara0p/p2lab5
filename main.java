472b1aee41f289b76eba95fc438f337f08d05faf
Sara0p <120247344+Sara0p@users.noreply.github.com>
2023-05-15 10:49:45 +0300

    firstcomm

--- main.java
+++ main.java
@@ -1,1 +1,21 @@
+class main{
+    public static void main(String[] args) {
 
+        
+
+        list1.add("one");
+        list1.add("two");
+        list1.add("six");
+
+        list2.add("one");
+        list2.add("two");
+        list2.add("three");
+       
+
+        System.out.println("Output1 :: "+compareList(list1,list2));
+
+        two.add("ten");
+
+        System.out.println("Output2 :: "+compareList(list1,list2));
+    }
+}
