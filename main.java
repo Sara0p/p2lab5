67446e46b8d1199d0a26fdb010b5a31394e14722
Sara0p <120247344+Sara0p@users.noreply.github.com>
2023-05-21 06:27:56 +0300

    final

--- main.java
+++ main.java
@@ -1,21 +1,33 @@
-class main{
+import java.util.ArrayList;
+   class main{
+      /**
+     * @param args
+     */
     public static void main(String[] args) {
-
+            ArrayList<String> list1 = new ArrayList<>();
+            list1.set(0,"Hi");
+            list1.set(1,"Hello");
+            list1.set(2, "blank");
+            System.out.println(list1);
         
-
-        list1.add("one");
-        list1.add("two");
-        list1.add("six");
-
-        list2.add("one");
-        list2.add("two");
-        list2.add("three");
-       
-
-        System.out.println("Output1 :: "+compareList(list1,list2));
-
-        two.add("ten");
-
-        System.out.println("Output2 :: "+compareList(list1,list2));
+        
+            ArrayList<String> list2 = new ArrayList<>();
+            list2.set(0,"Hi");
+            list2.set(1,"Hello");
+            list2.set(2,"bloom");
+            System.out.println(list2);
+        
+        /*part 3 */
+            ArrayList<Boolean> list3 = new ArrayList<>();
+            int i; 
+             boolean result;
+            for (i=0;;i++) {
+               if (list1(i).equalsIgnoreCase(list2(i)) {   
+                 result = true; }
+            else{
+                result = false;}
+           list3.add(result) ;
+           System.out.println("copare list 2 with 1"+list3);
+       }
     }
 }
